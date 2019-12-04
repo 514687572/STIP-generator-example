c0093263055446cc6a5936d4e2c8d08d787f97b5
chenjunan <chenjunan@sm.vvip-u.com>
2019-12-04 14:20:01 +0800

    更新为最新插件配置

--- src/main/java/com/stip/net/controller/TestController.java
+++ src/main/java/com/stip/net/controller/TestController.java
@@ -1,12 +1,12 @@
 package com.stip.net.controller;
 
+import com.stip.mybatis.generator.plugin.BaseExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stip.net.base.StipResponse;
-import com.stip.net.example.TestExample;
 import com.stip.net.service.TestService;
 
 @RequestMapping("/test")
@@ -18,7 +18,8 @@ public class TestController {
 	
 	@GetMapping("/getUserInfo")
     public StipResponse getUserInfo() {
-		TestExample example=new TestExample();
+		BaseExample example=new BaseExample();
+		example.createCriteria().addCriterion(" test_id =1");
 		testService.selectByExample(example);
 		
 		return StipResponse.success("success");
